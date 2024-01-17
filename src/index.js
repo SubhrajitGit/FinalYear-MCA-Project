909fe3e223d0e2f74a4d0f9fd059cbd9f573df07
SubhrajitGit <subhrajitdash43@gmail.com>
2024-01-17 16:31:35 +0530

    dayabase promise with app js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import express from "express"
-import {DB_NAME} from "./constants.js"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import app from "./app.js";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
-//DATABASE CONNECTION
-connectDB();
+    path: './env'
+});
 
-const port = process.env.PORT||4000
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Home Page')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is Running at Port: ${process.env.PORT || 4000}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB Connection Failed!!!", err);
+        process.exit(1); // Exit the process with an error code
+    });
