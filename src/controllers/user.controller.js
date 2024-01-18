93dc9f6c3bb62ec69becfdfff380564f6a8b8aee
SubhrajitGit <subhrajitdash43@gmail.com>
2024-01-18 20:21:57 +0530

    user login valid

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,23 @@
 import { asyncHandler } from "../utility/asyncHandler.js"
 import {ApiError} from "../utility/ApiError.js";
 import {User} from "../models/user.models.js"
+
+
+// const generateAccessTokenAndResfreshToken= async function(userId){
+//     try {
+//         const user = await User.findById(userId);
+//         const acessToken=user.generateAccessToken();
+//         const refreshToken=user.generateRefreshToken();
+        
+//         user.refreshToken=refreshToken;
+//         await user.save({ValidateBeforeSave: false});
+
+//         return {acessToken,refreshToken};
+//     } catch (error) {
+//         console.log("Something Went Wrong",error)
+//     }
+// }
+
 const registerUser = asyncHandler( async (req,res)=>{
 
     let { username,email,password,number }=req.body;//details from website
@@ -28,7 +45,7 @@ const registerUser = asyncHandler( async (req,res)=>{
             }
             else{
                 res.status(500).json({
-                    message:"User Register Fsil Server Error"
+                    message:"User Register Fail Server Error"
                 })
             }
         }
@@ -41,12 +58,28 @@ const registerUser = asyncHandler( async (req,res)=>{
 const loginUser = asyncHandler( async (req,res)=>{
     let { email,password }=req.body;
     try {
-        let findUser= await User.find({
-            $and:[{ email },{ password }]//and:Mongodb operator
-        })
-        if(findUser){
-
+        let findUser= await User.findOne({ email })
+        if(!findUser){
+            return res.status(404).json({
+                message:"User Does Not Exist"
+            })
         }
+        const isPasswordCorrect=await findUser.isPasswordCorrect(password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message:"Password Does Not Match"
+            })
+        }
+        else{
+            return res.status(200).json({
+                message:"Login Sucessfull"
+            })
+        }
+
+        // const { acessToken,refreshToken } = await generateAccessTokenAndResfreshToken(user._id);
+
+        // await User.findById(user._id);
+
     } catch (error) {
         console.log("Login Failed")
     }
