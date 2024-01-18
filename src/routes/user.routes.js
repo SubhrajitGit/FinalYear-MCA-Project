1faef3b1f13fa4d39f459210d6f4b6492630a238
SubhrajitGit <subhrajitdash43@gmail.com>
2024-01-18 20:22:36 +0530

    standard user route change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { loginUser, registerUser } from "../controllers/user.controller.js";
 
 const router=Router()
 
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/api/v1/register").post(registerUser)
+router.route("/api/v1/login").post(loginUser)
 
 export default router
